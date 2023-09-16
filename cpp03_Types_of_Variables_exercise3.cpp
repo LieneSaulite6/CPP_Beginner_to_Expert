272f9869f275f4687dcbc1309207951b77a541cb
Liene Saulīte <84623531+LieneSaulite6@users.noreply.github.com>
2023-09-16 14:11:10 +0300

    Add files via upload

---
+++ cpp03_Types_of_Variables_exercise3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+Exercise 3.
+
+Let's assume that we are creating an application for a hotel management.  At this stage of development we need to prepare variables for keeping most important information about rooms. Declare variables which will contain: room number, floor level, first name of person renting, last name of person renting, information about meals (if included, true or false). Choose proper variable type and use self descriptive names.
+
+*/
+
+main ()
+{
+    int roomNumber;
+    short floorLevel;
+    string renterName, renterLastName;
+    bool mealsIncluded;
+}
