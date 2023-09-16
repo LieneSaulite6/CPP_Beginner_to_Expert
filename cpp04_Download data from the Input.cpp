ddabd796c07d0c079939ad62af797105e11911d0
Liene Saulīte <84623531+LieneSaulite6@users.noreply.github.com>
2023-09-16 14:28:16 +0300

    Add files via upload

---
+++ cpp04_Download data from the Input.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+main ()
+{
+    /*
+    int a;
+    cin >> a; //console input
+    cout << "variable named a = " << a;
+    */
+
+    string name, surname;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Enter your surname: ";
+    cin >> surname;
+
+    cout << "Welcome " << name << " " << surname;
+}
