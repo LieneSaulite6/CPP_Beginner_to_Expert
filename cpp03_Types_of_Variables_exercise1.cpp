e03e114653425e4fb8cf0c17d57ca23e25b1143a
Liene Saulīte <84623531+LieneSaulite6@users.noreply.github.com>
2023-09-16 13:11:08 +0300

    Add files via upload

---
+++ cpp03_Types_of_Variables_exercise1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+Exercise 1.
+
+Declare one variable for each of the basic type (char, int, short, float, double), name them with the letters from alphabet starting from a. For each of the variable write a comment with information about the number of bytes reserved and the minimum - maximum value range of given type.
+
+eg.  
+char a;   //character 1 byte -128 to 127
+
+*/
+
+main ()
+{
+    char a;   //character 1 byte -128 to 127
+    int b;    //integer 4 bytes -2147483648 to 2147483647
+    short c;  //short integer 2 bytes -32768 to 3276
+    float d;  //floating point 4 bytes +/- 3.4e +/- 38 (~7 digits)
+    double e; //double floating point 8 bytes +/- 1.7e +/- 308 (~15 digits)
+}
