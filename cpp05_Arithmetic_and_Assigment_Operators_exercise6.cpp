0cb86633717808dd323b633735fdf3d2364323f9
Liene Saulīte <84623531+LieneSaulite6@users.noreply.github.com>
2023-09-17 16:37:11 +0300

    Add files via upload

---
+++ cpp05_Arithmetic_and_Assigment_Operators_exercise6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+Exercise 6.
+
+Write a program that asks the user to input two numbers and calculates their quotient (divison).
+Print the result in the console.
+
+*/
+
+main ()
+{
+    double number1, number2, number3;
+
+    cout << "Enter the number you want to divide:" << endl;
+    cin >> number1;
+
+    cout << "How many times?" << endl;
+    cin >> number2;
+
+    number3 = number1 / number2;
+
+    cout << "YOUR RESULT IS " << number3 << endl;
+}
