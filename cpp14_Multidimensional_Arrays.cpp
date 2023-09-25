9cf7d25a82e0f9958f33d8e71dd60f7ede8d8749
Liene Saulīte <84623531+LieneSaulite6@users.noreply.github.com>
2023-09-25 09:28:48 +0300

    Add files via upload

---
+++ cpp14_Multidimensional_Arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+//multidimensional arrays
+
+// int x[4] you create a container for 4 variables
+// int x[4][3] you create 4 containers with 3 variables
+
+main()
+{
+    int biArray [3][4] = {0}; //{0} resets all values in a given array
+
+    //int tab [4] = {1, 545, 424, 12} setting values during the definition of an array
+
+    cout << biArray [0][0] << endl;
+}
